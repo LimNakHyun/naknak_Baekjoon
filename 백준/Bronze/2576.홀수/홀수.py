d0891373031ff6e2731fa15eeb039ffb5d2acee0
LimNakHyun <97713696+LimNakHyun@users.noreply.github.com>
2022-02-09 08:30:03 +0900

    [Bronze III] Title: 홀수, Time: 68 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Bronze/2576.홀수/홀수.py
+++ 백준/Bronze/2576.홀수/홀수.py
@@ -6,11 +6,6 @@ for i in range(7):
         odd_sum=odd_sum+num
         if num<min_num:
             min_num=num
-        else:
-            pass
-    else:
-        pass
-
 if odd_sum!=0:
     print(odd_sum)
     print(min_num)
