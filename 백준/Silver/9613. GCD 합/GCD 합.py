73f68b7cc94c84df13091677caaf5637ab3c8e64
LimNakHyun <97713696+LimNakHyun@users.noreply.github.com>
2022-04-28 10:23:35 +0900

    [Silver III] Title: GCD 합, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/9613. GCD 합/GCD 합.py
@@ -0,0 +1,12 @@
+def gcd(m,n):
+    if  n>m:m,n=n,m
+    if m%n==0:return n
+    return gcd(n,m%n)
+
+for i in range(int(input())):
+    _sum=0
+    m,*arr=map(int,input().split())
+    for j in range(m):
+        for k in range(j+1,m):
+            _sum=_sum+gcd(arr[j],arr[k])
+    print(_sum)
