40072e17f95a3ed6add770e314ce75fa28405b8b
LimNakHyun <97713696+LimNakHyun@users.noreply.github.com>
2022-04-29 09:55:55 +0900

    [Bronze I] Title: 팰린드롬인지 확인하기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
@@ -1,10 +1,2 @@
-n_list=list(input())
-nbool='1'
-if len(n_list)==1:
-    nbool='1'
-else:
-    for i in range((len(n_list)//2)):
-        if n_list[i]!=n_list[len(n_list)-1-i]:
-            nbool='0'
-            break
-print(nbool)
+n=input()
+print(1) if n==n[::-1] else print(0)
