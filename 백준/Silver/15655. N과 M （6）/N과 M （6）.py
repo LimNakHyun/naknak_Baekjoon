08e831fdd69df6d981abffe147dde2f41e23a3e7
LimNakHyun <97713696+LimNakHyun@users.noreply.github.com>
2022-05-02 11:12:32 +0900

    [Silver III] Title: N과 M (6), Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.py
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -2,5 +2,4 @@ from itertools import combinations
 n,m=map(int,input().split())
 a=list(map(int,input().split()))
 b=list(combinations(sorted(a),m))
-# print(*b,sep='\n')
 for i in b:print(i[0]) if i==1 else print(*i)
