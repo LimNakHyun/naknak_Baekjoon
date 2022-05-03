2c844d2bb81119ec38b22238f96a04e6f1d2da13
LimNakHyun <97713696+LimNakHyun@users.noreply.github.com>
2022-05-04 01:26:04 +0900

    [Silver IV] Title: 덱, Time: 96 ms, Memory: 32416 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.py
+++ 백준/Silver/10866. 덱/덱.py
@@ -1,48 +1,7 @@
 from collections import deque
 import sys
 input=sys.stdin.readline
-
-n=int(input().strip())
-m=deque([])
-
-for _ in range(n):
-    ope=list(input().strip().replace('_',' ').split())
-
-    if ope[0]=='push':
-        if ope[1]=='front':
-            m.appendleft(ope[2])
-        if ope[1]=='back':
-            m.append(ope[2])
-    
-    if ope[0]=='pop':
-        if ope[1]=='front':
-            if len(m)==0:
-                print(-1)
-            else:
-                print(m.popleft())
-        if ope[1]=='back':
-            if len(m)==0:
-                print(-1)
-            else:
-                print(m.pop())
-
-    if ope[0]=='size':
-        print(len(m))
-        
-    if ope[0]=='empty':
-        if len(m)==0:
-            print(1)
-        else:
-            print(0)
-        
-    if ope[0]=='front':
-        if len(m)==0:
-            print(-1)
-        else:
-            print(m[0])
-        
-    if ope[0]=='back':
-        if len(m)==0:
-            print(-1)
-        else:
-            print(m[-1])
+a=deque()
+for _ in range(int(input().strip())):
+    b=list(input().split())    
+    a.appendleft(b[1]) if b[0]=='push_front' else a.append(b[1]) if b[0]=='push_back' else (print(-1) if not a else print(a.popleft())) if b[0]=='pop_front' else (print(-1) if not a else print(a.pop())) if b[0]=='pop_back' else print(len(a)) if b[0]=='size' else (print(1) if not a else print(0)) if b[0]=='empty' else print(-1) if not a else print(a[0]) if b[0]=='front' else (print(-1) if not a else print(a[-1]))
