4e107974b8f5eaf81ab822f27c1aaefd2c6b51c0
LimNakHyun <97713696+LimNakHyun@users.noreply.github.com>
2022-05-04 02:13:13 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 92 ms, Memory: 32372 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -3,6 +3,6 @@ n,k=map(int,input().split())
 a=deque([i+1 for i in range(n)])
 res=[]
 while a:
-    for _ in range(k-1):a.append(a.popleft())
+    a.rotate(-k+1)
     res.append(str(a.popleft()))
 print('<'+', '.join(res)+'>')
