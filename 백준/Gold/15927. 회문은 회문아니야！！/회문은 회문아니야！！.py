492df36a6598ab64e03d012fc7ba4dbe3c2f9d46
LimNakHyun <97713696+LimNakHyun@users.noreply.github.com>
2022-05-25 12:31:12 +0900

    [Gold V] Title: 회문은 회문아니야!!, Time: 72 ms, Memory: 31328 KB -BaekjoonHub

--- 백준/Gold/15927. 회문은 회문아니야！！/회문은 회문아니야！！.py
+++ 백준/Gold/15927. 회문은 회문아니야！！/회문은 회문아니야！！.py
@@ -1,8 +1,2 @@
 string=input()
-
-if string!=string[::-1]:
-    print(len(string))
-elif string.count(string[0])==len(string):
-    print(-1)
-else:
-    print(len(string)-1)
+print(len(string)) if string!=string[::-1] else print(-1) if string.count(string[0])==len(string) else print(len(string)-1)
