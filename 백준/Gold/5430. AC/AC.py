0706c17547fdaa845195579df1950a5a958d10f2
LimNakHyun <97713696+LimNakHyun@users.noreply.github.com>
2022-05-27 20:51:10 +0900

    [Gold V] Title: AC, Time: 292 ms, Memory: 46448 KB -BaekjoonHub

---
+++ 백준/Gold/5430. AC/AC.py
@@ -0,0 +1,31 @@
+from collections import deque
+import sys
+input=sys.stdin.readline
+
+for _ in range(int(input())):
+
+    funcs=list(input().strip())
+
+    n=int(input())
+    m=input().strip()[1:-1].split(',')
+    dq=deque(m)
+    if n==0:dq=[]
+    
+    rev=0
+    dcount=funcs.count('D')
+    continu=True
+
+    for func in funcs:
+        if len(dq)==0:
+            break
+        elif func=='R':
+            rev=rev+1
+        elif func=='D':
+            if rev%2==0:
+                dq.popleft()
+            else:
+                dq.pop()
+
+    if rev%2!=0:dq.reverse()
+
+    print('['+','.join(list(dq))+']') if n>=dcount else print('error')
