98ed2ffb317342afa4a8fc725223f37bc5cf8202
LimNakHyun <97713696+LimNakHyun@users.noreply.github.com>
2023-07-28 13:08:43 +0900

    [Gold IV] Title: 오큰수, Time: 236 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <stack>
+typedef std::pair<int, int> Node;
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+
+    std::stack<Node> stk;
+
+    int A;  // 수열의 크기 A
+    std::cin >> A;
+
+    int* ans = new int[A] {0,};
+
+    for (int i=0; i<A; i++)
+    {
+        int now;
+        std::cin >> now;
+        if (stk.empty())
+        {
+            stk.push(Node(i, now));
+        }
+        else
+        {
+            int temp_top = stk.top().second;
+            if (now > temp_top)
+            {
+                while (now > temp_top)
+                {
+                    ans[stk.top().first] = now;
+                    stk.pop();
+                    if (stk.empty()) break;
+                    else temp_top = stk.top().second;
+                }
+                stk.push(Node(i, now));
+            }
+            else
+            {
+                stk.push(Node(i, now));
+            }
+        }
+    }
+
+    for (int i=0; i<A; i++)
+    {
+        if (ans[i] != 0)
+        {
+            std::cout << ans[i] << " ";
+        }
+        else
+        {
+            std::cout << -1 << " ";
+        }
+    }
+
+    delete[] ans;
+
+    return 0;
+}
